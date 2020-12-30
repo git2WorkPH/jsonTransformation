95f41073cad1f53593dd46e645a95251d4c8d786
jc cervantes <jaycac@gmail.com>
2020-12-31 09:27:57 +1100

    merge functionality using lodash

--- index.ts
+++ index.ts
@@ -27,7 +27,37 @@ const basedInput : BaseMenu =
         end_time:'21:55',
     },
     categories:[{id: 37, title:'Hard Tacos' },{id:38,title:'Soft Tacos'}],
-    products:[{id:877,title:'One Hard Taco',categoryIds:[37],fillings:[]}],
+    products:[
+              {id:877,
+               title:'One Hard Taco',
+               categoryIds:[37],
+               price:0,
+               fillings:[{
+                   id:673,
+                   title:'Please select second Taco choice',
+                   price:0,
+                   fillings:[{
+                       id:748,
+                       title:'Grilled Chicken (Mild)',
+                       price:5
+                   }]
+                }]},
+              {id:783,
+               title:'Two Hard Taco',
+               description:'Two Hard Taco',
+               categoryIds:[37],
+               fillings:[{
+                   id:673,
+                   title:'Please select second Taco choice',
+                   price:0,
+                   fillings:[{
+                       id:2011,
+                       title:'Ground Beef (Spicy) Taco #1',
+                       price:4.75
+                   }]
+               }]},
+
+            ],
     }
 
     
@@ -37,7 +67,6 @@ if(!fs.existsSync('./output')){
 
 fs.writeFile('./output/nodeJsonTransform.json',JSON.stringify( NodeJsonTransform(basedInput),null,3),(err)=>{ if(err) console.log(err) } )  
 fs.writeFile('./output/json2JsonTransform.json',JSON.stringify( Json2JsonTransformer(basedInput),null,3),(err)=>{if(err) console.log(err)} )  
-
 
 
 
--- node-json-transform.ts
+++ node-json-transform.ts
@@ -1,25 +1,23 @@
 import { BaseMenu } from ".";
+import * as _ from 'lodash'
+import * as fs from 'fs';
+import { resolve } from "path";
+import { reject } from "lodash";
 
 const {transform} = require('node-json-transform')
 
-export function NodeJsonTransform(basedInput:BaseMenu) {
+export  function NodeJsonTransform(basedInput:BaseMenu) {
 
-  // explore filtering functionality, where were 2 json will merge and filter 
-  // merge json files
-  // replace original json with new json? process 2
-  // join different json structure (e.g store, categories:{ fillings:{}},products)
+// explore filtering functionality, where were 2 json will merge and filter 
+// merge json files 
+// replace original json with new json? process 2
+// join different json structure (e.g store, categories:{ fillings:{}},products)
 
-const storeId = {storeId:102}
+// TODO:
+// think on how to dynamically set the attributes in the template.
+// 
 
-const mergeSubItem = {
-  fillings:{
-    categories:[{
-      id:1,
-      price:0,
-      name:'add-ons'
-    }]
-  }
-}
+const store = {storeId:102}
 
 const storeDetails = {
    storeId:"store.id",
@@ -28,17 +26,20 @@ const storeDetails = {
     endTime:"store.end_time"
 }
 
-const categories = {
-  "item":{
-    "catId":"id",
-    "description":"title"
+const mergeProductFilling = {
+  id:1000,
+  description:'merge product filling',
+  price:0
+}
+
+const productsItem = 
+  { 
+    id:"products.id",
+    description:"products.title",
+    categoryIds:"products.categoryIds",
+    fillings:'product.fillings',
   }
-}
-
-const productFilling = {
-
-}
-
+  
 const template = {
     item:{
       service_availability:storeDetails,
@@ -46,26 +47,59 @@ const template = {
         items:'products',
         categories:'categories'
       }
+    },
+  operate:[
+    {
+      run:function(ar:any[]){
+        return transform(ar,productsItem)
+      },
+      on:'subsections.items'
+    },
+    {
+      run:()=>{return setStoreId()},
+      on:'service_availability.storeId'
+    },
+    {
+      run:(items:any[]) =>{
+          return productFillingMerge(items)
+      },
+      on:'subsections.items'
     }
-  ,  
-  
-  // this can be define in the UI (if any)
-  // remove:["id","phoneNumber","address"],
-  // each:function(item:any,store:any){
-  //   item.service_availability.storeId = store.id
-  //   return item
-  // }
+  ]
 };
 
+const result = JSON.stringify(transform(basedInput,template),null,3)
 
-function changeName(item:any):any{
 
-
+function productFillingMerge(ar:any[]){
+   return  ar.map(el => {
+      
+        if(el.title === 'One Hard Taco'){
+          el.fillings.push(mergeProductFilling)
+        }
+        return el
+    })
 }
-const result = transform(basedInput,template)
+
+function setStoreId(){
+  return store.storeId
+}
+
+// const json = new Promise(function(res){ fs.readFileSync('./output/nodeJsonTransform2.json','utf8',(data,err)=>{
+  
+//   if(err) console.log('err',err)
+//   console.log('1',data)
+
+//   res(data)
+//  })})
+const json = fs.readFileSync('./output/nodeJsonTransform2.json','utf8')
+console.log('test',json)
+console.log('transform',result)
+const mergeJson = JSON.stringify( _.merge(json,result),null,3)
+
+console.log('resultxx',mergeJson)
 
 
 
-
-return result;
+return mergeJson;
 }
